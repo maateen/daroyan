7c744f652cf2d21956a87327308870652adc3032
Maksudur Rahman Maateen <ugcoderbd@gmail.com>
2016-08-29 22:45:34 +0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+config = {
+        'database_name' : 'Database Name',
+        'database_user' : 'Database User',
+        'database_password' : 'Database Password',
+        'database_host' : 'localhost',
+        'cloudflare_api_key' : 'Your CloudFlare API Key',
+        'cloudflare_email_address' : 'Your CloudFlare Email Address',
+        'ban_time' : 'Time in seconds, a IP should be banned or challenged',
+        'action_interval' : 'Time in seconds, Daroyan should wait between two action',
+        'fail2ban_log' : 'Fail2Ban Log Path for Daroyan',
+        'daroyan_error_log' : 'Daroyan Error Log Path'
+}
